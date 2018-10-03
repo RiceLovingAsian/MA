e2ad2c39b6a930e50fb365d87321a66c69298389
Jonas Konrath <jonas.konrath@icloud.com>
2018-10-03 18:27:08 +0200

    updated scraping part

--- ReMake/ricardo_scraping_2.py
+++ ReMake/ricardo_scraping_2.py
@@ -27,6 +27,6 @@ for x in range(1,90):
 			break
 		else:
 			for a in articles:
-				car = rst.run(a["href"])
+				car = rst.run('https://auto.ricardo.ch/'+str(a["href"]))
 				print(car.comblist)
 				#print(car.markestring,car.modelstring)
